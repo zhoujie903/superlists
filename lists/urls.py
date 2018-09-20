d5895db656e88268071f2c7d872c63a64d91658b
zhoujie <zhoujie_903@163.com>
2018-09-20 09:26:51 +0800

    new URL + view for adding to existing lists. FT passes :-)

--- lists/urls.py
+++ lists/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from lists.views import home_page
+
+from django.urls import path
+from lists.views import view_list, new_list, add_item
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('', home_page),
-    path('lists/', include('lists.urls')),
+    path('<int:list_id>/', view_list),
+    path('<int:list_id>/add_item', add_item),
+    path('new', new_list),
+
 ]
