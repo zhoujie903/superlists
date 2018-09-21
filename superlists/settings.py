51a0ca51019be6de9af53eb1bc15d7170e2219ab
zhoujie <zhoujie_903@163.com>
2018-09-21 11:59:15 +0800

    解决部署到线上出现Invalid HTTP_HOST header: 'jason.pythonanywhere.com'. You may need to add 'jason.pythonanywhere.com' to ALLOWED_HOSTS.的问题

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tw6%!#sf&$gn-d3mxzj_zlz=uj)hb03v90l=ewtmli@@zsz9w%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
