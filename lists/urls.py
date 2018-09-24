7e928667eec650d81e581614be77d0d6d86e56b1
zhoujie <zhoujie_903@163.com>
2018-09-24 09:08:43 +0800

    Refactor hadr-coded URLs out of templates

--- lists/urls.py
+++ lists/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from lists.views import view_list, new_list
 
 urlpatterns = [
-    path('<int:list_id>/', view_list),
-    path('new', new_list),
+    path('<int:list_id>/', view_list, name='view_list'),
+    path('new', new_list, name='new_list'),
 
 ]
