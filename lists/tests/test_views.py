10c2c3be489c7ad49a3f1b4039bcaeecb598d55c
zhoujie <zhoujie_903@163.com>
2018-09-25 09:43:31 +0800

    use new form in home_page, simplify test.

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,22 +5,19 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.views import home_page
 from lists.models import Item, List
+from lists.forms import ItemForm
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    maxDiff = None
 
-    def test_home_page_returns_corrent_html(self):
-        request = HttpRequest()
-        response = home_page(request)
 
-        expected_html = render_to_string('home.html')         
-        # self.assertEqual(response.content.decode(), expected_html)
+    def test_home_page_renders_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-        # self.assertTrue(response.content.startswith(b'<html>'))
-        # self.assertIn(b'<title>To-Do lists</title>', response.content)
-        # self.assertTrue(response.content.strip().endswith(b'</html>'))
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 
 class ListViewTest(TestCase):
