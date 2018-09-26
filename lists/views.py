717dd51fa64c750a6d9ad5dd5562322d9a76a7e3
zhoujie <zhoujie_903@163.com>
2018-09-26 11:54:43 +0800

    Implement duplicate item validation at model layer

--- lists/views.py
+++ lists/views.py
@@ -15,7 +15,6 @@ def view_list(request, list_id):
     if request.method == 'POST':
         form = ExistingListItemForm(for_list=list_, data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
             form.save()
             return redirect(list_)
     return render(request, 'list.html', {'list': list_, 'form': form})
